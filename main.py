d50d22a68b3db5211f1689ae48dfd2122c51d43c
shuail0 <11707571@qq.com>
2023-08-29 16:10:11 +0800

    新增是否和上一个任务相同的判断

--- main.py
+++ main.py
@@ -8,18 +8,25 @@ import config
 # 使用Pandas打开CSV文件，并根据开始行数和结束行数读取部分数据
 df = pd.read_csv(config.input_file, skiprows=range(1, config.start_row), nrows=config.end_row)
 
+# 初始化一个用于保存新taskTag的列表
+new_taskTags = []
+
+# 遍历DataFrame以生成新的taskTags
+for index, row in df.iterrows():
+    old_taskTag = row['taskTag']
+    random_num = np.random.randint(config.lower_bound, config.upper_bound + 1)
+    
+    # 检查新生成的random_num是否与旧的taskTag或exclude_numbers相同
+    while random_num == old_taskTag or random_num in config.exclude_numbers:
+        random_num = np.random.randint(config.lower_bound, config.upper_bound + 1)
+    
+    new_taskTags.append(random_num)
+
+# 将新生成的taskTags赋值给DataFrame的taskTag列
+df['taskTag'] = new_taskTags
+
 # 使用sample函数对DataFrame进行随机打乱顺序
 df_shuffled = df.sample(frac=1, random_state=random.seed())
-
-# 为df_shuffled新增一列名为taskTag的列，生成的随机数需要检查是否在exclude_numbers中
-taskTags = []
-for _ in range(len(df_shuffled)):
-    random_num = np.random.randint(config.lower_bound, config.upper_bound + 1)
-    while random_num in config.exclude_numbers:
-        random_num = np.random.randint(config.lower_bound, config.upper_bound + 1)
-    taskTags.append(random_num)
-
-df_shuffled['taskTag'] = taskTags
 
 # 保存打乱顺序后的DataFrame为新的CSV文件
 df_shuffled.to_csv(config.output_file, index=False)
